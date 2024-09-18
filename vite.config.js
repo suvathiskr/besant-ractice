3c8a303bf152f3545740d791f6699a221fc46a74
suvathiskr <suvathiskr@gmail.com>
2024-09-18 10:48:49 +0530

    fd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: './',
+//   plugins: [react()],
+  
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  
+  css: {
+    minify: false,
+  },
 })
